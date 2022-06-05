76f0b50181234b95c59e4bf50bdf1feb302572be
trusilov <oleksandr.trusilov@gmail.com>
2022-06-05 21:01:20 +0300

    Added os activity functional

--- os_activities.py
+++ os_activities.py
@@ -1,7 +1,25 @@
 import os
+
 import static
 
-
 user = os.getlogin()
-path = 'C:/Users/{0}/Documents/{1}'.format(user, static.FOLDER)
-os.makedirs(path)
+folder_path = 'C:/Users/{0}/Documents/{1}'.format(user, static.FOLDER)
+file_path = '{0}/Keeper.json'.format(folder_path)
+
+
+def CreateFile(file):
+    f = open(file, "w+")
+    f.close()
+
+
+def CreateFolder(folder):
+    try:
+        os.makedirs(folder)
+    except FileExistsError:
+        print("This folder already exists")
+
+
+
+
+CreateFolder(folder_path)
+CreateFile(file_path)
